14e7e0c4150d7672c45015c21e89be03ad68807d
Paul Steele <paul@paul-steele.com>
2026-02-05 20:04:54 -0500

    prevent trees / dripstone

---
+++ src/main/java/com/bmp/jigsaw/expander/mixins/TreeFeatureMixin.java
@@ -0,0 +1,53 @@
+package com.bmp.jigsaw.expander.mixins;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.StructureManager;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
+import net.minecraft.world.level.levelgen.feature.TreeFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
+import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.StructureStart;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@SuppressWarnings("deprecation")
+@Mixin(TreeFeature.class)
+public class TreeFeatureMixin {
+
+    @Unique
+    private static boolean isCobbledCitiesStructure(WorldGenLevel level, Structure structure) {
+        var registry = level.registryAccess().registryOrThrow(Registries.STRUCTURE);
+        ResourceLocation key = registry.getKey(structure);
+        return key != null && key.getNamespace().equals("cobbled-cities");
+    }
+
+    @Inject(method = "place", at = @At("HEAD"), cancellable = true)
+    private void preventTreeInStructures(FeaturePlaceContext<TreeConfiguration> context,
+                                          CallbackInfoReturnable<Boolean> cir) {
+        WorldGenLevel level = context.level();
+        BlockPos pos = context.origin();
+
+        StructureManager structureManager = level.getLevel().structureManager();
+        ChunkPos chunkPos = new ChunkPos(pos);
+
+        for (StructureStart start : structureManager.startsForStructure(
+                chunkPos,
+                structure -> isCobbledCitiesStructure(level, structure))) {
+
+            BoundingBox box = start.getBoundingBox();
+
+            if (box.inflatedBy(8).isInside(pos)) {
+                cir.setReturnValue(false);
+                return;
+            }
+        }
+    }
+}
